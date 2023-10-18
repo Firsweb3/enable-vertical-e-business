0a593ecaa59ff59a77eeb807ef3dca721088060b
Firsweb3 <139719787+Firsweb3@users.noreply.github.com>
2023-10-18 08:50:50 +0300

    Update code_2.py

--- code_2.py
+++ code_2.py
@@ -1,56 +1,39 @@
-    data = [random.randint(1, 100) for _ in range(10)]
 
-    return data
-
-if __name__ == "__main__":
-def main():
-    main()
 
     for item in data:
-
 import random
-    data = generate_random_data()
         print(f"Random Number: {item}")
-def generate_random_data():    main()
-
-    data = generate_random_data()
-import random
-
+    data = [random.randint(1, 100) for _ in range(10)]
 if __name__ == "__main__":
 
-def generate_random_data():
-    data = [random.randint(1, 100) for _ in range(10)]
-    return data
-        print(f"Random Number: {item}")
-    for item in data:
 def main():
-def generate_random_data():
-def main():
-        print(f"Random Number: {item}")
-
-
-    for item in data:
     return data
-    data = generate_random_data()
-    data = [random.randint(1, 100) for _ in range(10)]
-import random
     main()
+def generate_random_data():
 
-
-if __name__ == "__main__":        data.push(Math.floor(Math.random() * 100) + 1);
-function main() {
-    });
-    return data;
-main();
-    }
-    data.forEach(item => {
-
+    data = generate_random_data()    }
+        int[] data = new int[10];
+            data[i] = (int) (Math.random() * 100) + 1;
+        }
+public class RandomDataGenerator {
+        }
+            System.out.println("Random Number: " + item);
+        for (int i = 0; i < 10; i++) {
 }
-        console.log("Random Number: " + item);
+        for (int item : data) {
+
+    public static void main(String[] args) {
+        print(f"Random Number: {item}")
+import random
+    data = generate_random_data()
+    main()
+def generate_random_data():
 
 
-function generateRandomData() {
-    const data = [];
-    const data = generateRandomData();
-}
-    for (let i = 0; i < 10; i++) {
+
+    for item in data:
+    data = [random.randint(1, 100) for _ in range(10)]
+    return data
+if __name__ == "__main__":
+
+def main():
